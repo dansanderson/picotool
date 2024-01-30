6622c97950ffb47bebe508c9e9aa8cc513465635
Draguve <ritwikpatil1998@gmail.com>
2024-01-30 15:47:18 -0800

    Added remaining assignment operators

--- pico8/lua/parser.py
+++ pico8/lua/parser.py
@@ -9,7 +9,6 @@ from typing import List
 from .. import util
 from . import lexer
 
-
 __all__ = [  # noqa: F822
     'Parser',
     'ParserError',
@@ -96,7 +95,7 @@ class Node():
                 pos = self._add_token_group(
                     (fieldname, inner_i), inner, tokenlist, pos)
         else:
-            self._token_groups.append(tokenlist[pos:pos+1])
+            self._token_groups.append(tokenlist[pos:pos + 1])
             pos += 1
         return pos
 
@@ -226,11 +225,11 @@ for (name, fields) in _ast_node_types:
         for k in kwargs:
             setattr(self, k, kwargs[k])
 
+
     cls = type(name, (Node,), {'__init__': node_init,
                                '_name': name, '_fields': fields,
                                '_children': None})
     globals()[name] = cls
-
 
 # (!= is PICO-8 specific.)
 BINOP_PATS = (tuple([lexer.TokSymbol(sym) for sym in [
@@ -305,15 +304,15 @@ class Parser():
         while True:
             cur_tok = self._peek()
             if (cur_tok is None or
-                cur_tok.matches(tok_pattern) or
-                (not isinstance(cur_tok, lexer.TokSpace) and
-                 not isinstance(cur_tok, lexer.TokNewline) and
-                 not isinstance(cur_tok, lexer.TokComment))):
+                    cur_tok.matches(tok_pattern) or
+                    (not isinstance(cur_tok, lexer.TokSpace) and
+                     not isinstance(cur_tok, lexer.TokNewline) and
+                     not isinstance(cur_tok, lexer.TokComment))):
                 break
             self._pos += 1
 
         if (cur_tok is not None and
-            cur_tok.matches(tok_pattern) and
+                cur_tok.matches(tok_pattern) and
                 (self._max_pos is None or self._pos < self._max_pos)):
             self._pos += 1
             return cur_tok
@@ -435,8 +434,18 @@ class Parser():
                          self._accept(lexer.TokSymbol(b'-=')) or
                          self._accept(lexer.TokSymbol(b'*=')) or
                          self._accept(lexer.TokSymbol(b'/=')) or
+                         self._accept(lexer.TokSymbol(b'\=')) or
                          self._accept(lexer.TokSymbol(b'%=')) or
-                         self._accept(lexer.TokSymbol(b'..=')))
+                         self._accept(lexer.TokSymbol(b'^=')) or
+                         self._accept(lexer.TokSymbol(b'..=')) or
+                         self._accept(lexer.TokSymbol(b'|=')) or
+                         self._accept(lexer.TokSymbol(b'&=')) or
+                         self._accept(lexer.TokSymbol(b'^^=')) or
+                         self._accept(lexer.TokSymbol(b'<<=')) or
+                         self._accept(lexer.TokSymbol(b'>>=')) or
+                         self._accept(lexer.TokSymbol(b'>>>=')) or
+                         self._accept(lexer.TokSymbol(b'<<>=')) or
+                         self._accept(lexer.TokSymbol(b'>><=')))
             if assign_op is not None:
                 explist = self._assert(self._explist(),
                                        'Expected expression in assignment')
@@ -475,7 +484,7 @@ class Parser():
 
             then_pos = self._pos
             if (self._accept(lexer.TokKeyword(b'then')) is None and
-                self._accept(lexer.TokKeyword(b'do')) is None and
+                    self._accept(lexer.TokKeyword(b'do')) is None and
                     (self._tokens[exp._end_token_pos - 1] == lexer.TokSymbol(b')'))):
                 # Check for PICO-8 short form.
 
@@ -668,7 +677,7 @@ class Parser():
         """
         exp_prefix = self._prefixexp()
         if (isinstance(exp_prefix, VarName) or
-            isinstance(exp_prefix, VarAttribute) or
+                isinstance(exp_prefix, VarAttribute) or
                 isinstance(exp_prefix, VarIndex)):
             return exp_prefix
         return None
@@ -920,8 +929,8 @@ class Parser():
 
         full_exp = self._prefixexp()
         if (full_exp is None or
-            (not isinstance(full_exp, FunctionCall) and
-             not isinstance(full_exp, FunctionCallMethod))):
+                (not isinstance(full_exp, FunctionCall) and
+                 not isinstance(full_exp, FunctionCallMethod))):
             self._pos = pos
             return None
         return full_exp
